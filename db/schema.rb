efd502503e90fd4c546ed8eceab28bb69fff7671
Hannah Broucaret <broucarethannah@gmail.com>
2022-01-08 18:37:06 +0100

    Just a test"

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_01_08_150413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "baskets", force: :cascade do |t|
     t.boolean "paid", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
-    
-    
+
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
     t.datetime "created_at", precision: 6, null: false
