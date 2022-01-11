f1504cbde2b0cfb01ea1210f3d4a59e344d17e15
victor plantevin <victor.plantevin@gmail.com>
2022-01-11 20:46:40 +0100

    Nft home

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,2 +1,24 @@
 class NftsController < ApplicationController
+  def index
+    @nft = Nft.all
+  end
+
+  def new
+    @nft = Nft.new
+  end
+
+  def create
+    @nft = Nft.new(nft_params)
+    if @nft.save
+      redirect_to nft_path(@nft)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def nft_params
+    params.require(:list).permit(:title, :description, :price, :category_id)
+  end
 end
