e327d53ea2bd12751a5d7e7e16d95b0bc40deaf9
Hannah Broucaret <broucarethannah@gmail.com>
2022-01-11 21:14:58 +0100

    basket controller and some views

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,2 +1,30 @@
 class BasketsController < ApplicationController
+  def index
+    @baskets = Basket.all
+  end
+
+  def new
+    @basket = Basket.new
+  end
+
+  def create
+    @basket = Basket.new(basket_params)
+    @basket.user = current_user.id
+    if @basket.save
+      redirect_to baskets_path(@basket)
+    else
+      redirect_to nfts_path
+    end
+  end
+
+  def destroy
+    @basket = Basket.find(params[:id])
+    @basket.destroy
+  end
+
+  private
+
+  def basket_params
+    params.require(:basket, :basket.user).permit(:paid)
+  end
 end
