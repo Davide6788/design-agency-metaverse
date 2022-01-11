71d1fb4a1fab1568c53e58d8eb81bec3e2871127
Hannah Broucaret <broucarethannah@gmail.com>
2022-01-11 11:24:43 +0100

    dedroom -> bedroom

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  validates :category_name, presence: true, inclusion: { in: %w(kitchen dedroom desk arts&crafts ) }
+  validates :category_name, presence: true, inclusion: { in: %w(kitchen bedroom desk arts&crafts ) }
 end
