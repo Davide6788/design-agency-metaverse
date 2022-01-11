7c490d80a9d862da6d201485357d1619a4cc86b9
Matt Clark <clarkm02@gmail.com>
2022-01-11 20:38:23 +0100

    added actions to NftsController

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -4,4 +4,27 @@ class NftsController < ApplicationController
     @nft = Nft.find(params[:id])
   end
 
+  def new
+    @nft = Nft.new
+  end
+
+  def create
+    @nft = Nft.new(nft_params)
+  end
+
+  def update
+    @nft = Nft.find(params[:id])
+    @nft.update(params[:nft])
+  end
+
+  def destroy
+    @nft = Nft.find(params[:id])
+    @nft.destroy
+  end
+
+  private
+
+  def nft_params
+    params.require(:nft).permit(:title, :description, :price, :rented)
+  end
 end
