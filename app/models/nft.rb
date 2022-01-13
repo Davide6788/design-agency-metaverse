fed38ba775cb42ef174bd92efea25a2d6b8ad934
davide osella <oselladavide@gmail.com>
2022-01-13 23:09:05 +0100

    changed Nft model to reflect payment, amended new nft form

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -1,8 +1,7 @@
 class Nft < ApplicationRecord
   validates :title, :description, :price, presence: true
   has_one_attached :photo
-  # validates :rented, presence: true
-  # belongs_to :category
+  belongs_to :category
   belongs_to :user
   has_one :basket
 end
