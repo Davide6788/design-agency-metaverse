9a4692f526977dbc05ea09ff88936020324935d0
davide osella <oselladavide@gmail.com>
2022-01-14 15:54:16 +0100

    created paid and pending actions and created views to show your own basekts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :baskets, only: [:show]
+  resources :baskets do
+    collection do
+      get :paid, :pending
+    end
+  end
+
+  resources :baskets, only: [:show, :index]
 
   resources :nfts do
     resources :baskets, only: [:create, :new]
