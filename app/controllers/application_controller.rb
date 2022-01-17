a296d7f5f5e7b83f472f27fafb74d03043a61696
victor plantevin <victor.plantevin@gmail.com>
2022-01-17 23:41:35 +0100

    this is the NFT show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-
   before_action :authenticate_user!
+  after_action :allow_algolia_iframe
 
+  private
+
+  def allow_algolia_iframe
+    response.headers.delete('X-Frame-Options')
+  end
 end
