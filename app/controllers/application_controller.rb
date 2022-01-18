8eca3c0724cd0f062944eaf88aa100966396da62
victor plantevin <victor.plantevin@gmail.com>
2022-01-18 20:23:38 +0100

    NFT SHOW + Category Index + Solve conflicts + Delete Algolia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  after_action :allow_algolia_iframe
-
-  private
-
-  def allow_algolia_iframe
-    response.headers.delete('X-Frame-Options')
-  end
 end
